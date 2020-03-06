9020d096c970c96e9f4a125f341b9c53c3ee5379
Ashutosh Tripathi <ashuuu38@gmail.com>
2020-03-05 21:33:28 -0800

    second commit

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
@@ -7,6 +8,11 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request,'home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogApp/home.html'
+    context_object_name = 'posts'
 
 def about(request):
     return render(request,'about.html', {'title':'About'})
